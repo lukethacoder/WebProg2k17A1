ac474415e4433d9541934af1b47d3c34b99862d5
Luke Secomb <luke.secomb@live.com>
2017-09-18 22:46:08 +1000

    intitial commit

---
+++ cnbr_food_a1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cnbr_food_a1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Restaurants()
+        {
+            ViewData["Message"] = "Restaurants page. Provide information about local Canberra restaurants";
+
+            return View();
+        }
+
+        public IActionResult Cuisine()
+        {
+            ViewData["Message"] = "Cuisine page.";
+
+            return View();
+        }
+
+        public IActionResult Dishes()
+        {
+            ViewData["Message"] = "Dishes page.";
+
+            return View();
+        }
+
+        public IActionResult Price()
+        {
+            ViewData["Message"] = "Dishes page.";
+
+            return View();
+        }
+
+        public IActionResult Location()
+        {
+            ViewData["Message"] = "Location page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
